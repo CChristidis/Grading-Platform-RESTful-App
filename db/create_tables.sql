7251b6daef55973eac87afd385e156f2614aa590
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-05-05 21:48:09 +0300

    Update create_tables.sql

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -21,8 +21,8 @@ primary key(id)
 CREATE TABLE Enrolled
 (idS INT NOT NULL,
  idC INT NOT NULL,
- project_grade INT NOT NULL,
- final_exam_grade INT NOT NULL,
+ project_grade FLOAT NOT NULL,
+ final_exam_grade FLOAT NOT NULL,
  foreign key(idS) REFERENCES Student(id) ON DELETE CASCADE ,	 
  foreign key(idC) REFERENCES Course(id) ON DELETE CASCADE 	
 );
