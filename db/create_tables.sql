bf2ff57c904809d7d03880cdaa0148fa4688b60c
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-05-06 22:00:10 +0300

    Update create_tables.sql

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -12,18 +12,9 @@ primary key(id)
 );
 
 CREATE TABLE student_registration
-(id INT NOT NULL AUTO_INCREMENT,
+(
 name VARCHAR(255) NOT NULL,
 year_of_registration INT NOT NULL,
 semester INT NOT NULL,
-primary key(id)
-);
-
-CREATE TABLE enrolled
-(idS INT NOT NULL,
- idC INT NOT NULL,
- project_grade FLOAT NOT NULL,
- final_exam_grade FLOAT NOT NULL,
- foreign key(idS) REFERENCES student_registration(id) ON DELETE CASCADE ,	 
- foreign key(idC) REFERENCES course(id) ON DELETE CASCADE 	
+foreign key(idC) REFERENCES course(id) ON DELETE CASCADE 	
 );
