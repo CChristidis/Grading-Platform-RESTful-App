8b2d993a94cfa68bf5148f78b20951cc8c2c7d49
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-05-09 03:18:33 +0300

    Update create_tables.sql

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,6 +1,12 @@
 CREATE DATABASE grading_platform;
 USE grading_platform;
 
+DROP TABLE IF EXISTS `student_registration`;
+DROP TABLE IF EXISTS `course`;
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
+
 CREATE TABLE course
 (id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(255) NOT NULL,
@@ -9,7 +15,7 @@ instructor_login VARCHAR(255) NOT NULL,
 year INT NOT NULL,
 semester INT NOT NULL,
 primary key(id)
-);
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;;
 
 CREATE TABLE student_registration
 (
@@ -22,7 +28,28 @@ project_grade DOUBLE,
 final_exams_grade DOUBLE,
 foreign key(idC) REFERENCES course(id) ON DELETE CASCADE,
 primary key(id)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;;
+
+
+
+CREATE TABLE users (
+    username varchar(50) not null primary key,
+    password varchar(120) not null,
+    enabled boolean not null
 );
+
+CREATE TABLE authorities (
+    username varchar(50) not null,
+    authority varchar(50) not null,
+    foreign key (username) references users (username) ON DELETE CASCADE
+);
+
+
+INSERT INTO users(username, password, enabled)VALUES('christos','heygoodlookin',true);
+INSERT INTO users(username, password, enabled)VALUES('makis','whatyougotcookin',true);
+ 
+INSERT INTO authorities(username,authority)VALUES('christos','ROLE_ADMIN');
+insert into authorities(username,authority)values('makis','ROLE_USER');
 
 INSERT INTO `course` VALUES 
 	(1,'course1','syllabus','leslie@luv2code.com', 2022, 5),
