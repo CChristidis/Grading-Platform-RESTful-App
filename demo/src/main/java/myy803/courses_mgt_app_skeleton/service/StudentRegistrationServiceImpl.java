6b0f849b89e4050a870b322b1339cce3adb5b644
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-05-12 03:27:56 +0300

    Add files via upload

---
+++ demo/src/main/java/myy803/courses_mgt_app_skeleton/service/StudentRegistrationServiceImpl.java
@@ -0,0 +1,79 @@
+package myy803.courses_mgt_app_skeleton.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import myy803.courses_mgt_app_skeleton.dao.StudentRegistrationDAO;
+import myy803.courses_mgt_app_skeleton.entity.Course;
+import myy803.courses_mgt_app_skeleton.entity.StudentRegistration;
+
+@Service
+public class StudentRegistrationServiceImpl implements StudentRegistrationService {
+	@Autowired
+	private StudentRegistrationDAO studentRegistrationRepository;
+	
+	public StudentRegistrationServiceImpl() {
+		super();
+	}
+
+	@Autowired
+	public StudentRegistrationServiceImpl(StudentRegistrationDAO theStudentRegistrationRepository) {
+		studentRegistrationRepository = theStudentRegistrationRepository;
+	}
+	
+
+	
+	@Override
+	@Transactional
+	public List<StudentRegistration> findAll() {
+		return studentRegistrationRepository.findAll();
+	}
+	
+	
+
+	
+	@Override
+	@Transactional
+	public List<StudentRegistration> findRegistrationByCourseId(int theId) {
+		List<StudentRegistration> result = studentRegistrationRepository.findRegistrationByCourseId(theId);
+		
+		if (result != null )
+			return result;
+		else
+			throw new RuntimeException("Did not find student registration with id - " + theId);
+	}
+
+	@Override
+	@Transactional
+	public void delete(int theId) {
+		studentRegistrationRepository.deleteById(theId);
+		
+	}
+
+	@Override
+	@Transactional
+	public void save(StudentRegistration theStudentRegistration) {
+		studentRegistrationRepository.save(theStudentRegistration);
+	}
+	
+	public void setStudentRegistrationGrades(StudentRegistration theStudentRegistration, double projectGrade, double finalExamsGrade) {
+		theStudentRegistration.setProject_grade(projectGrade);
+		theStudentRegistration.setFinal_exams_grade(finalExamsGrade);
+	}
+
+	@Override
+	public StudentRegistration findById(int theId) {
+		StudentRegistration result = studentRegistrationRepository.findById(theId);
+		
+		if (result != null )
+			return result;
+		else
+			throw new RuntimeException("Did not find course with id - " + theId);
+	}
+
+
+}
