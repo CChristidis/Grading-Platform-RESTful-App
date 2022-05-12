586d7fb1c644b719a9dffcf3c249380a7417cb15
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-05-12 19:43:53 +0300

    Add files via upload

---
+++ demo/src/main/java/myy803/courses_mgt_app_skeleton/CourseAdministrationApplication.java
@@ -0,0 +1,16 @@
+package myy803.courses_mgt_app_skeleton;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CourseAdministrationApplication {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAdministrationApplication.class, args);
+	}
+}
