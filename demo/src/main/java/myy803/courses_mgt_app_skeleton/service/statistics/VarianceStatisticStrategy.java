ef3e3d0d8402f654e026e5c8661fd6b16ef7ee82
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-05-14 23:17:10 +0300

    Delete demo directory

--- demo/src/main/java/myy803/courses_mgt_app_skeleton/service/statistics/VarianceStatisticStrategy.java
+++
@@ -1,9 +0,0 @@
-package myy803.courses_mgt_app_skeleton.service.statistics;
-
-
-public class VarianceStatisticStrategy extends TemplateStatisticStrategy {
-
-	public double calculateStatistic() {
-		return descriptiveStatistics.getVariance();
-	}
-}
